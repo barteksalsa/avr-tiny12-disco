2021de95354902437f45ab85ca6aaf55c1271934
bsokol <barteksalsa@icloud.com>
2017-01-19 05:50:03 +0100

    correctly handling timer isr

--- go
+++ go
@@ -1,8 +1,12 @@
 rm -f leds.o leds.hex leds.elf
 
+echo "C compiler" &&\
 avr-gcc -ffunction-sections -fdata-sections -g -O4 -mmcu=at90s2313 -c leds.c &&\
+echo "C linker" &&\
 avr-gcc -Wl,--gc-sections -mmcu=attiny12 -o leds.elf leds.o &&\
+echo "C dumps" &&\
 avr-objcopy -j .text -O ihex leds.elf leds.hex &&\
-avr-objdump -h -d leds.elf > leds.objdump
+avr-objdump -h -d leds.elf > leds.objdump &&\
+echo "C done"
 
 #sudo avrdude -v -p t12 -c usbasp -Uflash:w:leds.hex -Ereset,vcc
--- leds.c
+++ leds.c
@@ -1,46 +1,149 @@
 #include <avr/io.h>
- 
-register unsigned char i asm("r3");
-register unsigned char y asm("r4");
-register unsigned char z asm("r5");
+#include <avr/interrupt.h>
 
-void delayLoop(void)
+register unsigned char pwmFastCount asm("r6");
+register unsigned char savedR24 asm("r7");
+register unsigned char savedR25 asm("r8");
+register unsigned char pwmFill asm("r9");
+register unsigned char counter asm("r10");
+register unsigned char counter2 asm("r11");
+
+
+/*
+ *  PINS in PORTB setup
+ */
+#define PWMOUTPIN  3
+#define DEBUGPIN   4
+
+/*
+ *  Using ACSR to return flags from Timer Interrupt.
+ *  Register variables are non-volatile by default, so hacking is needed
+ */
+
+#ifdef ACSR
+#undef ACSR
+#endif
+#define ACSR    _SFR_IO8(0x08)
+
+void clrFlagTimerTick(void)
 {
-    for (z = 100; z > 0; --z)
-    {
-        for (y = 255; y > 0; --y)
-        {
-            asm volatile ("nop"::);
-        }
-    }
+    ACSR &= ~_BV(0);
 }
+
+void setFlagTimerTick(void)
+{
+    ACSR |= _BV(0);
+}
+
+uint8_t getFlagTimerTick(void)
+{
+    return (ACSR & _BV(0));
+}
+
+
+
+
+/* timer interrupt handler */
+ISR(_VECTOR(3), ISR_NAKED)
+{
+    /* save r24, r25 */
+    asm("mov r7, r24"::);
+    asm("mov r8, r25"::);
+
+    /* rewind timer - must be first */
+    _SFR_IO8(0x32) = 256 - 117; /* TCNT0: need to count 25 ticks */
+
+    /* rest */
+    ++pwmFastCount;
+    if ((pwmFastCount & 32) == 32)
+    {
+        pwmFastCount = 0;
+        PORTB |= _BV(PWMOUTPIN);  /* enable PWM pin here */
+        setFlagTimerTick(); /* 300 Hz flag */
+    }
+
+    if (pwmFastCount >= pwmFill)
+    {
+        PORTB &= ~_BV(PWMOUTPIN); /* disable PWM pin when counter */
+    }
+
+    //PORTB ^= _BV(DEBUGPIN);
+    /* restore r24, r25 and return */
+    asm("mov r24, r7"::);
+    asm("mov r25, r8"::);
+    reti();
+}
+
+
 
 void setupTimer(void)
 {
-
+    _SFR_IO8(0x33) = 1; /* TCCR0 set timer to CLK/1 */
+    TIMSK = _BV(TOIE0);
 }
+
 
 void calibrateOscillator(void)
 {
-    _SFR_IO8(0x31) = 0x27; /* 0x27 read from avrdude -Ucalibrate */
+    _SFR_IO8(0x31) = 0x27; /* OSCCAL 0x27 read from avrdude -Ucalibrate */
 }
+
+void setupPwm(void)
+{
+    pwmFill = 2;
+}
+
+void setPwm(uint8_t newPwmFill)
+{
+    pwmFill = newPwmFill;
+}
+
+uint8_t getPwm(void)
+{
+    return pwmFill;
+}
+
+void incPwm(void)
+{
+    if (pwmFill < 32)
+    {
+        pwmFill = pwmFill + 1;
+    }
+    else
+    {
+        pwmFill = 0;
+    }
+}
+
 
 int main(void)
 {
     /* hack to provide minimum C runtime */
     asm volatile("eor	r1, r1"::);
-
+    DDRB = _BV(PB3) | _BV(PB4);
     calibrateOscillator();
     setupTimer();
+    setupPwm();
+    MCUCR |= _BV(SE);
+    sei();
 
-
-    DDRB = _BV(PB3) | _BV(PB4);                       // initialize port C
+    counter = 0;
     for (;;)
     {
-        PORTB = _BV(3) | _BV(4);            // PC0 = High = Vcc
-        delayLoop();
-        PORTB = 0;            // PC0 = Low = 0v
-        delayLoop();
+        if (getFlagTimerTick())
+        {
+            cli();
+            clrFlagTimerTick();
+            sei();
+            ++counter;
+        }
+
+        if (counter > 20)
+        {
+            incPwm();
+            counter = 0;
+        }
+        asm("sleep"::);
     }
 }
 
